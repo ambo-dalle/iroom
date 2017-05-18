bb9e366316406c7be0cf1af1df1de1e0a4ec2413
Hacktiv8 <hacktiv8-12@Hacktiv8-12.local>
2017-05-18 18:40:57 +0700

    coab d merge

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,6 +19,12 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('trust proxy', 1)
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,37 @@ var multer = require('multer');
 var upload = multer({
   dest : 'public/uploads'
 })
+var sequelize = require('sequelize')
 /* GET users listing. */
+router.use(function(req, res, next) {
+  let pathNeedLogin = ['/home', '/']
+  let pathNeedLogOut = ['/login', '/register']
+  console.log(`-------------------req.session.user = ${req.session.user}`)
+  let currentUser = req.session.user
+  console.log(`-------------------currentUser = ${currentUser}`)
+
+  let currentPath = req.path
+
+  if (pathNeedLogin.includes(currentPath)) {
+    if (!currentUser) {
+      res.render('login-page', {msg: "Login dlu coy!"})
+    } else {
+      next()
+    }
+  } else if (pathNeedLogOut.includes(currentPath)){
+    if (currentUser) {
+      res.redirect('/users/home')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
 router.get('/', function(req, res, next) {
   res.render('file_upload');
 });
-
-
 
 router.post('/', upload.any(), (req, res, next) => {
     let tempUrlPath = req.files[0].path
@@ -32,5 +57,87 @@ router.post('/', upload.any(), (req, res, next) => {
     // res.send(req.files)
 })
 
+router.get('/login', function(req, res, next) {
+  res.render('login-page.ejs', {msg: ""})
+})
+
+router.post('/login', function(req, res, next) {
+  let currentUser = req.body.logUsername;
+  let currentPass = req.body.logPassword;
+  models.User.findOne({
+    where: {
+      username: currentUser
+    }
+  }).then((user) => {
+    if (!user) {
+      res.render('login-page.ejs', {msg: "Username tidak di temukan bro!"})
+    } else if (user.password != currentPass) {
+      res.render('login-page.ejs', {msg: "Password salah bro!"})
+    } else if (user.password == currentPass) {
+      req.session.user = {username: currentUser}
+      console.log(`------------dari post login---------${req.session.user.username}`)
+      res.redirect('/users/home')
+    }
+  })
+})
+
+router.get('/logout', function(req, res, next) {
+  delete req.session.user
+  res.redirect('login')
+})
+
+router.get('/home', function(req, res, next) {
+  models.Rooms.findAll({
+      include : [models.Vote]
+    })
+    .then(rooms =>{
+      res.render('home-login', {
+        title : 'Daftar Tempat Penginapan',
+        rooms : rooms
+      })
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+})
+
+router.get('/register', function(req, res, next) {
+  res.render('regist-page.ejs', {msg: ""})
+})
+
+router.post('/register', function(req, res, next) {
+  let newName = req.body.newName
+  let newUsername = req.body.newUsername
+  let newPass = req.body.newPassword
+  let newEmail = req.body.newEmail
+  let newTelp = req.body.newTelp
+  models.User.findOne({
+    where: sequelize.or({
+      username: newUsername},{
+      email: newEmail
+    })
+  }).then( user => {
+    if (!user) {
+      models.User.create({
+        name: newName,
+        username: newUsername,
+        password: newPass,
+        email: newEmail,
+        telp: newTelp,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }).then( () => {
+        res.render('login-page', {msg: "Akun berhasil di buat, silahkan log in!"})
+      })
+    } else {
+      res.render('regist-page', {msg: "Username atau Password sudah terpakai!"})
+    }
+  })
+})
+
+router.post('/coba', function(req,res,next) {
+  let imagekah = req.body.imageUpload
+  console.log(imagekah)
+})
 
 module.exports = router;
